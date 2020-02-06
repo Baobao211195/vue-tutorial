094f8d8cc0a954477d47dc3e2a8f4691623d76fd
pvoanh <pvoanh@cmc.com.vn>
2020-02-06 11:56:44 +0700

    Conditional rendering : use Key properties

--- src/components/ConditionalRendering.vue
+++ src/components/ConditionalRendering.vue
@@ -5,17 +5,33 @@
 		<h3 v-else>Use v-else</h3>
 	</div>
 </template>
+<template >
+	<div>
+		<div v-if="isLogin">
+			<label>Username</label>
+			<input placeholder="Enter your username" key="username-input">
+		</div>
+		<div v-else>
+			<label>Email</label>
+			<input placeholder="Enter your email address" key="email-input">
+		</div>
+		<input type="button" @click="hasLogin" value="toggle login">
+	</div>
+</template>
 
 <script>
     export default {
         name: "ConditionalRendering",
 		data() {
             return {
-				isActive: false
+				isActive: false,
+                isLogin: true
 			}
 		},
-		computed: {
-
+		methods: {
+            hasLogin: function () {
+				return this.isLogin = !this.isLogin;
+            }
 		}
     }
 </script>
