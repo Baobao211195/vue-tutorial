c82da224f0fff21464966be2ab9baa711fc8d565
pvoanh <pvoanh@cmc.com.vn>
2020-02-12 09:08:29 +0700

    delete one component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
     <div id="app">
         <img alt="Vue logo" src="./assets/logo.png">
-        <demo1></demo1>
         <computed-properties></computed-properties>
         <syntax></syntax>
         <attribute></attribute>
