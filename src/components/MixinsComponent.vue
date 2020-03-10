329ededaf5c2d105769ed5abfcddfc068793ab87
pvoanh <pvoanh@cmc.com.vn>
2020-03-10 11:18:20 +0700

    take priority when have conflict keys in object

--- src/components/MixinsComponent.vue
+++ src/components/MixinsComponent.vue
@@ -25,23 +25,23 @@
     export default {
         name: "MixinsComponent",
         mixins: [myMixin],
-        data: function () {
-            return {
-                message: 'hello',
-                bar: 'def'
-            }
-        },
-        created: function () {
-            console.log("create from component!")  // => { message: "goodbye", foo: "abc", bar: "def" }
-            console.log("data is merge from mixins :======"
-				+ this.message
-				+ '===' + this.foo
-                + '===' + this.bar)  // => { message: "goodbye", foo: "abc", bar: "def" }
-
-        },
+        // data: function () {
+        //     return {
+        //         message: 'hello',
+        //         bar: 'def'
+        //     }
+        // },
+        // created: function () {
+        //     console.log("create from component!")  // => { message: "goodbye", foo: "abc", bar: "def" }
+        //     console.log("data is merge from mixins :======"
+		// 		+ this.message
+		// 		+ '===' + this.foo
+        //         + '===' + this.bar)  // => { message: "goodbye", foo: "abc", bar: "def" }
+		//
+        // },
         methods: {
             hello: function () {
-                console.log('methods from component!')
+                console.log('Hello from component!') //  sẽ được ưu tiên khi có cùng method trong mixins
             }
         }
     }
