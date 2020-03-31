e6da988491b1e0038df0c177609564cef5d08dd0
pvoanh <pvoanh@cmc.com.vn>
2020-03-31 11:08:28 +0700

    add fallback contents in the slots

--- src/components/slot/SlotParentComponent.vue
+++ src/components/slot/SlotParentComponent.vue
@@ -1,10 +1,20 @@
 <template>
 	<div>
+		<!--Renders from parent-->
 		<slot-child-component>
-			<p>I'm injected content from the parent!,
+			<p>
+				I'm injected content from the parent!,
 				I can still bind to data in the parent's scope, like this! {{myVariable}}
 			</p>
 		</slot-child-component>
+
+		<!-- Renders "Hello from the child!" -->
+		<slot-child-component>
+			There are not content from parent
+		</slot-child-component>
+
+		<!--nếu ở parent ko inject bất kỳ content nào vào child'slot thì
+		child sẽ render bất kỳ tag nào trong <slot></slot>-->
 	</div>
 
 </template>
