ce4fb8c3a8934ab19fd61fd737450c54f861f562
pvoanh <pvoanh@cmc.com.vn>
2020-04-03 14:29:12 +0700

    add plug in:

---
+++ src/components/plugins/PluginComponent.vue
@@ -0,0 +1,24 @@
+<template>
+	<!--
+	luôn sử dụng ở mức global
+	sử dụng trong 1 vài case sau :
+	+ Thêm một vài method or properties global (eg.vue-custom-element -> https://github.com/karol-f/vue-custom-element)
+	+ Thêm một hoặc nhiều asset global  directives/filters/transitions etc. e.g. vue-touch -> https://github.com/vuejs/vue-touch
+	+ Thêm một vài component option bởi global mixin (eg.vue-router-> https://github.com/vuejs/vue-router)
+	+ Add some Vue instance methods by attaching them to Vue.prototype.
+	+ A library that provides an API of its own, while at the same time injecting some combination of the above. e.g. vue-router
+	-->
+	<div>
+
+	</div>
+</template>
+
+<script>
+    export default {
+        name: "PluginComponent"
+    }
+</script>
+
+<style scoped>
+
+</style>
